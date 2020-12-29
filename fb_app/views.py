9f687a5ac023456868075a9d0f71cda1fbab097e
jflynn87 <39394930+jflynn87@users.noreply.github.com>
2020-12-29 17:18:24 +0900

    analytics fix

--- fb_app/views.py
+++ fb_app/views.py
@@ -767,9 +767,9 @@ class AllTeamResults(APIView):
 
             league_data = stats.all_team_results()
             player_data = [{k: {'wrong': d['wrong'], 'right': d['right'], 'win_percent': "{:.0%}".format(round(int(d['right'])/(int(d['right'])+int(d['wrong'])),2))} for k, d in player_stats.items()}]
-            print (type(player_data), len(player_data), player_data)
+            #print (type(player_data), len(player_data), player_data)
             sorted_player_data = {k: v for k, v in sorted(player_data[0].items(), key=lambda item: item[1]['win_percent'])}
-            print (sorted_player_data)
+            #print (sorted_player_data)
             data = {
                 'player_data': sorted_player_data,
                 'league_data': league_data
