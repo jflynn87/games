05f10d6c8e12510f933a82c27ac700042f1332f1
jflynn87 <39394930+jflynn87@users.noreply.github.com>
2022-11-20 18:52:26 +0900

    fix sort

--- static/wc_app/js/scores.js
+++ static/wc_app/js/scores.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var start = new Date()
     fetch("/wc_app/wc_scores_api",         
     {method: "GET",
     })
@@ -31,16 +32,19 @@ $(document).ready(function () {
 
                 $('#' + user + '_row').append('<td id=' + user + group.substr(-1)  + '_cell><table class=table-sm id=' + user + group.substr(-1)  + 
                                                     '_cell_tbl><tr class="small all_watermark"><td>Team</td><td>Pick</td><td>Score</td></tr></tabke>' + '</td>')
-                $.each(pick, function(team, team_d) {
+                var groupTbl = buildGroupTable(pick, user, group ,$('#' + user + group.substr(-1)  + '_cell_tbl'))
+                groupTbl.then((response) => {sortGroup(user + group.substr(-1)  + '_cell_tbl')})
+                
+                //                            $.each(pick, function(team, team_d) {
                                                 
-                                                if (team_d == 'perfect picks') {
+                //                                 if (team_d == 'perfect picks') {
                                                     
-                                                    $('#' + user + group.substr(-1) + '_cell_tbl').css('background-color', 'lightgreen')}
-                                                else {
-                                                $('#' + user + group.substr(-1) + '_cell_tbl').append(
-                                                '<tr><td><img src=' + team_d.flag +' style=height:20; width:20;>' + team  + '</td><td>' + team_d.pick_rank + '</td><td>' + team_d.points +  '</td></tr>' + 
-                                                '</table>' ) }
-                                             })
+                //                                     $('#' + user + group.substr(-1) + '_cell_tbl').css('background-color', 'lightgreen')}
+                //                                 else {
+                //                                 $('#' + user + group.substr(-1) + '_cell_tbl').append(
+                //                                 '<tr><td><img src=' + team_d.flag +' style=height:20; width:20;>' + team  + '</td><td>' + team_d.pick_rank + '</td><td>' + team_d.points +  '</td></tr>' + 
+                //                                 '</table>' ) }
+                //                              })
                 }
             
             
@@ -48,10 +52,11 @@ $(document).ready(function () {
     
     }) //closes 1st each
     
-    console.log('sorting')
+    //console.log('sorting')
     sort_table('score_table')
     
     $('#status').html('<p class=small>Scores Updated - Teams are listed in ranked order per group, green background for any perfect groups<p>')
+    console.log('Duration: ', new Date() - start)
 
   } //closes else
 }) //closes then    
@@ -91,3 +96,57 @@ function sort_table(tableId) {
         }
       }
     }
+
+
+function buildGroupTable(pick, user, group, tableId) {
+  return new Promise(function (resolve,reject) {
+  $.each(pick, function(team, team_d) {
+                                                
+    if (team_d == 'perfect picks') {
+        
+        $('#' + user + group.substr(-1) + '_cell_tbl').css('background-color', 'lightgreen')}
+    else {
+    $('#' + user + group.substr(-1) + '_cell_tbl').append(
+    '<tr><td hidden>' + team_d.team_rank + '</td><td><img src=' + team_d.flag +' style=height:20; width:20;>' + team  + '</td><td>' + team_d.pick_rank + '</td><td>' + team_d.points +  '</td></tr>' + 
+    '</table>' ) }
+ })
+ resolve()
+})
+
+}
+
+function sortGroup(tableId) {
+  var table, rows, swtiching, i, x, y, shouldSwitch;
+  table = $('#' + tableId)
+  //console.log(table)
+  switching = true;
+  
+  while(switching) {
+    switching = false;
+    rows = table[0].rows;
+    //console.log(rows.length)
+    l = rows.length
+    for (i=0; i < (l-1); i++) {
+      //console.log('for loop', i, rows[i])
+      shouldSwitch = false;
+      
+      x = rows[i].getElementsByTagName('td')[0].innerHTML;
+      //console.log('x', x)
+      y = rows[i + 1].getElementsByTagName('td')[0].innerHTML;
+      //console.log('xy ' ,x, y)
+      
+      if (Number(x) > Number(y)) {
+        //console.log(i, Number(x), Number(y))
+        shouldSwitch = true;
+        break;
+      }
+    }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+
+
+
